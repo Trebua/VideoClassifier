8c29cc19e2b730d9b4979a0c0ebe0ea5e3c15e0b
Sindre Aarnes Aubert <sindreaubert@gmail.com>
2018-09-23 14:43:40 +0200

    Små endringer

--- TEDCollection.py
+++ TEDCollection.py
@@ -2,6 +2,7 @@ from TEDGet import download_ted #tar inn dwn_link (url fra dataset) og to_path (
 import csv
 import json
 import ast
+from math import inf
 #import cPickle as pickle
 
 def read_csv():
@@ -28,13 +29,21 @@ def read_csv():
     f.close()
 
 #Rydder opp ratings og skriver de til path
+#Må endres: finner nå prosentandel for hver rating utifra den som har fått flest votes
 def write_ratings(ratings, to_path):
     lst = ast.literal_eval(ratings) #Evaluerer streng til liste og dictionary på en trygg måte
+    dict = {} #dict vil være på formen: kategori : [antall stemmer, prosentandel av antall stemmer]
+    max = -inf
 
-    #Dictionary med alle mulige ratings fra datasettet (TED har endret til andre ratings nå)
-    dict = {"Funny": 0, "Beautiful":0, "Ingenious":0, "Courageous":0, "Longwinded":0, "Confusing":0, "Informative":0, "Fascinating":0, "Unconvincing":0,"Persuasive":0,"Jaw-dropping":0,"OK":0,"Obnoxious":0,"Inspiring":0}
     for row in lst:
-        dict[row["name"]] = row["count"]
+        votes = row["count"]
+        category = row["name"]
+        max = votes if votes > max else max #Tar utgangspunkt i max og ikke total antall votes for å regne ut prosent
+        dict[category] = [votes,0]
+    
+    for key in dict:
+        dict[key][1] = dict[key][0]/max #finner prosentandel som har stemt på kategorien
+    
     with open(to_path, 'w') as file: #Kan kanskje gjøres raskere og letter reverserbart med pickle dumps og loads
         file.write(str(dict))
 
--- TEDGet.py
+++ TEDGet.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
-import wget
+import wget #må lastes ned med pip
 
 #Funksjonen lagrer video av LAV kvalitet (kan endres til høy eller medium) fra TED-talks
 #dwn_link:"https://www.ted.com/talks/ken_robinson_says_schools_kill_creativity"
